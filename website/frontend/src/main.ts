7c6f33f36af8416db66de4bd767b02dae9d17508
rChrisb <5767@holbertonstudents.com>
2024-04-17 00:10:48 +0200

    update to use vuex

--- website/frontend/src/main.ts
+++ website/frontend/src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import store from './stores/store' // Import your Vuex store
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(store) // Use Vuex store
 app.use(autoAnimatePlugin)
 
 app.mount('#app')
