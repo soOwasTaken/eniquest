78c10e0d23838f2838949023d59da27a9eaf166c
soOwasTaken <444deph12@gmail.com>
2024-04-19 19:35:04 +0200

    LandingPage

--- website/frontend/src/components/ClassicLogin.vue
+++ website/frontend/src/components/ClassicLogin.vue
@@ -3,6 +3,7 @@
   <div class="overlay" v-show="isVisible">
     <div class="overlay-content">
       <form @submit.prevent="handleSubmit" class="login-form">
+
         <div class="input-holder">
           <i class="icon far fa-user"></i>
           <input type="text" class="input" placeholder="Email" v-model="email" required />
@@ -10,18 +11,23 @@
         <div class="input-holder" v-if="mode === 'login' || mode === 'signup'">
           <i class="icon fas fa-lock"></i>
           <input type="password" class="input" placeholder="Password" v-model="password" required />
+
         </div>
         <div class="input-holder" v-if="mode === 'signup'">
           <i class="icon fas fa-lock"></i>
           <input type="password" class="input" placeholder="Confirm Password" v-model="confirmPassword" required />
         </div>
+
         <div class="input-holder">
-          <a v-if="mode === 'login'" class="link" @click="toggleMode">Don't have an account?</a>
-          <a v-else class="link" @click="toggleMode">Already have an account?</a>
-          <input type="submit" class="button" :value="mode === 'login' ? 'Log In' : 'Sign Up'" />
+          <input type="submit" class="button larger-button" :value="mode === 'login' ? 'Log In' : 'Sign Up'" />
         </div>
       </form>
     </div>
+        <div class="footer">
+        <p v-if="mode === 'login'">Don't have an account?</p>
+        <p v-else class="message-spacing">Already have an account?</p>
+        <button class="button" @click="toggleMode">{{ mode === 'login' ? 'Sign up' : 'Login' }}</button>
+      </div>
   </div>
 </template>
 
@@ -66,6 +72,16 @@ export default {
     },
     toggleVisibility() {
       this.isVisible =!this.isVisible;
+    },
+    mounted() {
+      // Define the animation on mount
+      anime({
+        targets: this.$refs.overlay,
+        opacity: [0, 1], // Start opacity at 0 and end at 1
+        translateY: ['-20px', '0px'], // Start translateY at -20px and end at 0px
+        duration: 800, // Animation duration in milliseconds
+        easing: 'easeInOutCubic', // Easing function for smooth transition
+      });
     }
     // ... other methods required for the overlay
   }
@@ -75,7 +91,6 @@ export default {
 <style scoped>
 .overlay {
   position: fixed;
-  padding: 40px;
   top: 45%;
   left: 60%;
   transform: translate(-50%, -50%);
@@ -83,12 +98,14 @@ export default {
   max-width: 500px; /* Max-width for better control */
   border: 0.5px solid #fff;
   border-radius: 8px;
+  background-color: rgba(0, 0, 0, 0.2);
 }
 
 
 .overlay-content {
   display: flex;
   flex-direction: column;
+  padding: 38px;
 }
 
 .login-form {
@@ -97,7 +114,9 @@ export default {
 
 
 .input-holder {
+  display: flex;
   position: relative;
+  justify-content: center;
   color: #ffffffc4;
   padding-top: 30px;
 }
@@ -130,11 +149,10 @@ export default {
 
 .button {
   width: auto;
-  float: right;
   padding: 8px 30px;
   background: transparent;
   color: #fff;
-  border: 2px solid white;
+  border: 1px solid white;
   font-family: 'Sedan', serif;
   border-radius: 5px;
   cursor: pointer;
@@ -147,13 +165,15 @@ export default {
 }
 
 .link {
-
   color: #ffffffb2;
   font-family: 'Sedan', serif;
-  font-size: 20px;
   cursor: pointer;
   text-decoration: none;
 
+}
+
+.larger-button {
+  font-size: 1.2rem; /* Adjust as needed */
 }
 
 
@@ -161,9 +181,19 @@ export default {
   color: #fff;
 }
 
+.footer {
+  font-family : 'Sedan' , serif;
+  font-size: 1.1rem;
+  display: flex;
+  justify-content: space-between;
+  padding:20px 50px 20px 50px;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  border-top : 0.5px solid #fff;
+}
 @media (max-width: 768px) {
   .overlay {
-
     width: 90%;
   }
 }
